c0add7e1c7d627261effae3c96bf309c2f195105
Andrew Orellano <orellano.andrew@gmail.com>
2022-07-08 15:44:44 -0700

    fixed all linting issues w/ flake8

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,10 +5,12 @@ Django command to wait for postgres db to be available
 import time
 from django.db import OperationalError
 
-# Operational error already exists so added the 'Op' in Psycopg2Error to differentiate the name
+# Operational error already exists so added the 'Op' in Psycopg2Error
+# to differentiate the name
 from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """Django command to wait for db"""
@@ -20,13 +22,14 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                #think self in this case is BaseCommand (from Django) check is a function in BaseCommand
-                # so self(basecommand).check --> checks the dbs in this case default but its an array so you could check multiple dbs
+                # think self in this case is BaseCommand (from Django) check is
+                # a function in BaseCommand
+                # so self(basecommand).check --> checks the dbs in this case
+                # default but its an array so you could check multiple dbs
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second..')
                 time.sleep(1)
-        # self style success just makes it green 
+        # self style success just makes it green
         self.stdout.write(self.style.SUCCESS('Database available!'))
-
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,18 +1,19 @@
 """
 Test custom django management commands
 """
-# this is the built in python unittest class with mock function that helps mimick certain functionality
-from email.policy import default
+# this is the built in python unittest class with mock function that
+# helps mimick certain functionality
 from unittest.mock import patch
 
-#type of error we might get if we try to connect to the DB before it's ready
-# this error happens in postgres when the server is trying to connect 
+# type of error we might get if we try to connect to the DB before it's ready
+# this error happens in postgres when the server is trying to connect
 from psycopg2 import OperationalError as Psycopg2Error
 
 # helper function to call the command we're testing
 from django.core.management import call_command
 # another error that can be thrown by the db
-# this one is specific to django when the database itself isn't ready it's a different error than the postgres error
+# this one is specific to django when the database itself
+# isn't ready it's a different error than the postgres error
 from django.db.utils import OperationalError
 # base test class from django
 from django.test import SimpleTestCase
@@ -29,12 +30,13 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
-    
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError"""
         # \ is a way to break the line to keep it clean
-        # the testing 2 then 3 times is arbitary just a rough estimate of what would happen in reality
+        # the testing 2 then 3 times is arbitary just a rough estimate
+        # of what would happen in reality
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
 
